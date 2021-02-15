515fc7d5e0cf576512665cfd2c087ad568b63195
swastikpatel <swastikpatel@192.168.0.17>
2021-02-14 19:01:57 -0800

    Test Commit

---
+++ MyFirstJavaProject1/src/SecondJavaPackage/TestClass2.java
@@ -0,0 +1,12 @@
+// 2_14_21
+package SecondJavaPackage;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
