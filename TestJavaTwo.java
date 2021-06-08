bfdd18fb69a822620bc8eb4c6f3a0481b8685d0e
Swastik-Patel1 <68673893+Swastik-Patel1@users.noreply.github.com>
2021-06-08 08:07:37 -0700

    2nd eclipse file

---
+++ TestJavaTwo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+// TestJavaTwo.java
+
+import java.util.Scanner;
+
+public class TestJavaTwo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String word = in.next();
+        System.out.println(word);
+    }
+}
